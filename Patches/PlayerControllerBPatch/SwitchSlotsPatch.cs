903ea0f7e1a4d849323805b767c3f759dc851f2c
Henit3 <henit33@gmail.com>
2024-02-18 22:56:24 +0000

    Prevented switching slots when mask is attached

---
+++ Patches/PlayerControllerBPatch/SwitchSlotsPatch.cs
@@ -0,0 +1,23 @@
+﻿using DramaMask.Extensions;
+using DramaMask.Network;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DramaMask.Patches.PlayerControllerBPatch;
+
+[HarmonyPatch(typeof(PlayerControllerB), "ScrollMouse_performed")]
+public class SwitchSlotsPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(PlayerControllerB __instance)
+    {
+        if (!__instance.isPlayerControlled) return true;
+
+        if (!__instance.IsLocal()) return true;
+
+        if (!NetworkHandler.Instance.MyPretend.IsMaskAttached) return true;
+
+        // Cancel event if all of the above conditions are passed
+        return false;
+    }
+}
