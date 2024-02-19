b7256a7d5d6d4ec8f874b94be6634fd779ea38ad
Henit3 <henit33@gmail.com>
2024-02-19 00:13:08 +0000

    Stopped discarding when mask attached

---
+++ Patches/PlayerControllerBPatch/BaseChangeItemPatch.cs
@@ -0,0 +1,20 @@
+﻿using DramaMask.Extensions;
+using DramaMask.Network;
+using GameNetcodeStuff;
+
+namespace DramaMask.Patches.PlayerControllerBPatch;
+
+public abstract class BaseChangeItemPatch
+{
+    protected static bool ShouldInvoke(PlayerControllerB __instance)
+    {
+        if (!__instance.isPlayerControlled) return true;
+
+        if (!__instance.IsLocal()) return true;
+
+        if (!NetworkHandler.Instance.MyPretend.IsMaskAttached) return true;
+
+        // Cancel event if all of the above conditions are passed
+        return false;
+    }
+}
--- Patches/PlayerControllerBPatch/DiscardItemPatch.cs
+++ Patches/PlayerControllerBPatch/DiscardItemPatch.cs
@@ -3,14 +3,12 @@ using HarmonyLib;
 
 namespace DramaMask.Patches.PlayerControllerBPatch;
 
-[HarmonyPatch(typeof(PlayerControllerB), "ScrollMouse_performed")]
-public class SwitchSlotsPatch : BaseChangeItemPatch
+[HarmonyPatch(typeof(PlayerControllerB), "Discard_performed")]
+public class DiscardItemPatch : BaseChangeItemPatch
 {
     [HarmonyPrefix]
     public static bool Prefix(PlayerControllerB __instance)
     {
-        if (__instance.inTerminalMenu) return true;
-
         return ShouldInvoke(__instance);
     }
 }
