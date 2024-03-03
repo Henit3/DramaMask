88e460a0db670e81b51d6e91818ced4f4964d417
Henit3 <henit33@gmail.com>
2024-03-03 15:56:52 +0000

    Prevent grabbing with mask attached

---
+++ Patches/PlayerControllerBPatch/GrabObjectPatch.cs
@@ -0,0 +1,14 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DramaMask.Patches.PlayerControllerBPatch;
+
+[HarmonyPatch(typeof(PlayerControllerB), "BeginGrabObject")]
+public class GrabObjectPatch : BaseChangeItemPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(PlayerControllerB __instance)
+    {
+        return ShouldInvoke(__instance);
+    }
+}
