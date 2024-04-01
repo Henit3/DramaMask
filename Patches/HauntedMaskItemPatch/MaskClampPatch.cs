f811060886cba76ac3c6d9e80918074a9024eee1
Henit3 <henit33@gmail.com>
2024-04-01 18:05:52 +0100

    Despawn worn mask if possessed

---
+++ Patches/HauntedMaskItemPatch/MaskClampPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DramaMask.Patches.HauntedMaskItemPatch;
+
+[HarmonyPatch(typeof(HauntedMaskItem), nameof(HauntedMaskItem.MaskClampToHeadAnimationEvent))]
+public class MaskClampPatch
+{
+    [HarmonyPrefix]
+    public static void Prefix(HauntedMaskItem __instance,
+        bool ___attaching, PlayerControllerB ___previousPlayerHeldBy)
+    {
+        // Mimic the original condition for possession
+        if (!___attaching || ___previousPlayerHeldBy == null) return;
+        if (__instance.currentHeadMask == null) return;
+
+        Object.Destroy(__instance.currentHeadMask.gameObject);
+        __instance.currentHeadMask = null;
+    }
+}
