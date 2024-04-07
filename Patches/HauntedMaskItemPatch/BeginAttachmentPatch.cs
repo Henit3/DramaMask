73dc096061a7a5df4e3d4a4c1411aee21d0208f7
Henit3 <henit33@gmail.com>
2024-04-07 18:39:43 +0100

    Config to stop attached masks possessing

---
+++ Patches/HauntedMaskItemPatch/BeginAttachmentPatch.cs
@@ -0,0 +1,15 @@
+﻿using DramaMask.Network;
+using HarmonyLib;
+
+namespace DramaMask.Patches.HauntedMaskItemPatch;
+
+[HarmonyPatch(typeof(HauntedMaskItem), nameof(HauntedMaskItem.BeginAttachment))]
+public class BeginAttachmentPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(HauntedMaskItem __instance)
+    {
+        return !NetworkHandler.Instance.MyPretend.IsMaskAttached
+            || Plugin.Config.AttachedCanPossess.Value;
+    }
+}
