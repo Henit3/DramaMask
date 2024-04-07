35ba4b38e0e7150fd5f503081aaac9a988e82551
Henit3 <henit33@gmail.com>
2024-04-07 14:58:37 +0100

    Apply BaseModifyPlayerArrayPatch to more EnemyAI methods

---
+++ Patches/EnemyAIPatch/GetAllPlayersInLineOfSightPatch.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DramaMask.Patches.EnemyAIPatch;
+
+[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.GetAllPlayersInLineOfSight))]
+public class GetAllPlayersInLineOfSightPatch : BaseModifyPlayerArrayPatch
+{
+    [HarmonyPrefix]
+    public static void Prefix(EnemyAI __instance,
+        float width = 45f, int range = 60, int proximityAwareness = -1, float bufferDistance = 0f)
+    {
+        SaveAndModifyPlayerArray(__instance);
+    }
+
+    [HarmonyPostfix]
+    public static void Postfix(EnemyAI __instance, ref PlayerControllerB __result,
+        float width = 45f, int range = 60, int proximityAwareness = -1, float bufferDistance = 0f)
+    {
+        LoadOriginalPlayerArray(__instance);
+    }
+}
