13fecbd67c61b1bd60948fdb39ed702a262c9a4b
Henit3 <henit33@gmail.com>
2024-04-08 00:58:00 +0100

    Patch to be able to hide from turrets

---
+++ Patches/TurretPatch/GetAllPlayersInLineOfSightPatch.cs
@@ -0,0 +1,58 @@
+﻿using DramaMask.Extensions;
+using DramaMask.Network;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace DramaMask.Patches.TurretPatch;
+
+[HarmonyPatch(typeof(Turret), nameof(Turret.CheckForPlayersInLineOfSight))]
+public class CheckForPlayersInLineOfSightPatch
+{
+    // Do not check if the player is hidden
+    private static readonly List<CodeInstruction> _isPlayerHidden = [
+        new(OpCodes.Ldloc_3),           // (PlayerControllerB) component
+        new(OpCodes.Call,               // CheckForPlayersInLineOfSightPatch.IsPlayerHidden
+            AccessTools.Method(typeof(CheckForPlayersInLineOfSightPatch), nameof(IsPlayerHidden)))
+    ];
+    private static bool IsPlayerHidden(PlayerControllerB player)
+    {
+        return NetworkHandler.Instance.VisiblePlayers != null
+            && !NetworkHandler.Instance.VisiblePlayers.Contains(player.GetId());
+    }
+
+    // Should only be called by the server so safe to use NetworkHandler.Instance.StealthMap
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> IgnoreHiddenPlayersPatch(IEnumerable<CodeInstruction> instructions,
+        ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // Go to end and match for "Player", where it stores any players found (main player loop predicate)
+        matcher.MatchForward(false, [new(OpCodes.Ldstr, "Player")]);
+        matcher.MatchForward(false, [new(OpCodes.Ldarg_0)]);
+
+        // Go back since the br type is not agreed upon to match for (dotPeek: brfalse.s; harmony: brfalse)
+        matcher.Advance(-1);
+
+        // Store the branch target to use in adjusted predicate
+        var skipPlayerTarget = matcher.Instruction.operand;
+
+        // Skip to the player assignment so we can insert our extra condition after this
+        matcher.MatchForward(true, [new(OpCodes.Stloc_3)]);
+        matcher.Advance(1);
+
+        // Skip dropping the active mask if a mask is attached and it is currently being held
+        // if (_isPlayerHidden) continue;
+        matcher.InsertAndAdvance(
+            _isPlayerHidden
+            .Union([
+                new(OpCodes.Brtrue,         // continue
+                    skipPlayerTarget)
+        ]));
+
+        return matcher.InstructionEnumeration();
+    }
+}
