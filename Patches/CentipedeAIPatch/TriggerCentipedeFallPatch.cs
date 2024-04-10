51a2e0cc3c32cf0fe090a4005b3e7f09d774dcbc
Henit3 <henit33@gmail.com>
2024-04-10 01:01:55 +0100

    Prevent snare flea falling if player hidden

---
+++ Patches/CentipedeAIPatch/TriggerCentipedeFallPatch.cs
@@ -0,0 +1,25 @@
+﻿using DramaMask.Extensions;
+using DramaMask.Network;
+using HarmonyLib;
+
+namespace DramaMask.Patches.CentipedeAIPatch;
+
+[HarmonyPatch(typeof(CentipedeAI), nameof(CentipedeAI.TriggerCentipedeFallServerRpc))]
+public class TriggerCentipedeFallPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(CentipedeAI __instance, ref bool ___triggeredFall,
+        ulong clientId)
+    {
+        if (NetworkHandler.Instance.VisiblePlayers != null
+            && !NetworkHandler.Instance.VisiblePlayers
+                .Contains(GameNetworkManager.Instance.localPlayerController.GetId()))
+        {
+            // Reset the triggeredFall state if cancelling the fall
+            ___triggeredFall = false;
+            return false;
+        }
+
+        return true;
+    }
+}
