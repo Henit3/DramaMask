5bf0393fd416c1a5f063fe9838695393149c8446
Henit3 <henit33@gmail.com>
2024-04-12 00:27:16 +0100

    Added patch to playerIsTargetable, ignoring collision detection

---
+++ Patches/EnemyAIPatch/PlayerIsTargetablePatch.cs
@@ -0,0 +1,22 @@
+﻿using DramaMask.Extensions;
+using GameNetcodeStuff;
+using HarmonyLib;
+using System.Diagnostics;
+
+namespace DramaMask.Patches.EnemyAIPatch;
+
+[HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))]
+public class PlayerIsTargetablePatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(EnemyAI __instance, ref bool __result, 
+        PlayerControllerB playerScript, bool cannotBeInShip = false, bool overrideInsideFactoryCheck = false)
+    {
+        // Don't want to invalidate player collision
+        var callingMethod = new StackFrame(1).GetMethod().Name;
+        if (nameof(EnemyAI.MeetsStandardPlayerCollisionConditions) == callingMethod) return true;
+
+        __result = playerScript.IsHidden();
+        return !__result;
+    }
+}
