8d714166e3e512c2a6b190ed8f1b2fac3d6d9715
Henit3 <henit33@gmail.com>
2024-04-13 21:01:02 +0100

    Transpile OOB in remaining ModifyPlayerArray derived patches

--- Patches/EnemyAIPatch/CheckLineOfSightForClosestPlayerPatch.cs
+++ Patches/EnemyAIPatch/CheckLineOfSightForClosestPlayerPatch.cs
@@ -1,6 +1,8 @@
 ﻿using DramaMask.Patches.Base;
 using GameNetcodeStuff;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 
 namespace DramaMask.Patches.EnemyAIPatch;
 
@@ -14,7 +16,25 @@ public class CheckLineOfSightForClosestPlayerPatch : ModifyPlayerArrayPatch
         SaveAndModifyPlayerArray(__instance);
     }
 
-    // TODO: Transpile OOB
+    // Out of bounds handling with static player count transpilation from other mods
+    [HarmonyAfter([
+        "notnotnotswipez-MoreCompany-1.8.1",
+        "bizzlemip-BiggerLobby-2.6.0",
+        "PotatoePet-AdvancedCompany-1.0.150"
+    ])]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> StopOutOfBounds(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // Match for last blt (main loop predicate)
+        matcher.End();
+        matcher.MatchBack(false, [new(OpCodes.Blt)]);
+
+        AddOobCheckToLoopPredicate(matcher, new(OpCodes.Ldloc_S, 4));
+
+        return matcher.InstructionEnumeration();
+    }
 
     [HarmonyPostfix]
     public static void Postfix(EnemyAI __instance, ref PlayerControllerB __result,
