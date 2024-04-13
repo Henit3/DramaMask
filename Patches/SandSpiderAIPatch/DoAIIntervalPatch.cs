919b3b9f487587d0334a8768d7f310ecfeae199d
Henit3 <henit33@gmail.com>
2024-04-13 01:26:56 +0100

    Added spider patches

---
+++ Patches/SandSpiderAIPatch/DoAIIntervalPatch.cs
@@ -0,0 +1,20 @@
+﻿using DramaMask.Patches.EnemyAIPatch;
+using HarmonyLib;
+
+namespace DramaMask.Patches.SandSpiderAIPatch;
+
+[HarmonyPatch(typeof(SandSpiderAI), nameof(SandSpiderAI.DoAIInterval))]
+public class DoAIIntervalPatch : BaseModifyPlayerArrayPatch
+{
+    [HarmonyPrefix]
+    public static void Prefix(SandSpiderAI __instance)
+    {
+        SaveAndModifyPlayerArray(__instance);
+    }
+
+    [HarmonyPostfix]
+    public static void Postfix(EnemyAI __instance)
+    {
+        LoadOriginalPlayerArray(__instance);
+    }
+}
---
+++ Patches/SandSpiderAIPatch/TriggerChaseWithPlayerPatch.cs
@@ -0,0 +1,22 @@
+﻿using DramaMask.Extensions;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DramaMask.Patches.SandSpiderAIPatch;
+
+[HarmonyPatch(typeof(SandSpiderAI), nameof(SandSpiderAI.TriggerChaseWithPlayer))]
+public class TriggerChaseWithPlayerPatch
+{
+    [HarmonyPrefix]
+    public static bool Prefix(SandSpiderAI __instance, ref bool __result,
+        PlayerControllerB playerScript)
+    {
+        // TODO: Optional behaviour to ignore this
+
+        if (!playerScript.IsHidden()) return true;
+
+        // Override with can't find the player if the player is hiding
+        __result = false;
+        return false;
+    }
+}
