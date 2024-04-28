4433d6d72f1223b2ae2d54702689616a8b200aa5
Henit3 <henit33@gmail.com>
2024-04-28 15:35:22 +0100

    Switched local player id source again for reliability

--- src/Extensions/PlayerControllerBExtensions.cs
+++ src/Extensions/PlayerControllerBExtensions.cs
@@ -1,11 +1,12 @@
 ﻿using DramaMask.Network;
 using GameNetcodeStuff;
+using Unity.Netcode;
 namespace DramaMask.Extensions;
 
 public static class PlayerControllerBExtensions
 {
     public static ulong GetId(this PlayerControllerB player) => player?.playerClientId ?? 0;
-    public static bool IsLocal(this PlayerControllerB player) => player.GetId() == HUDManager.Instance.localPlayer.playerClientId;
+    public static bool IsLocal(this PlayerControllerB player) => player.GetId() == NetworkManager.Singleton.LocalClientId;
 
     // We assume that the HoldMask animation has been overriden by the attach mask handler
     public static void SetArmsRaised(this PlayerControllerB player, bool isRaising) {} //player.SafeSetAnimation("HoldMask", isRaising);
