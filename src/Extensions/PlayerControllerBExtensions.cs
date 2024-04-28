007f2c3283847cbe8cc86cc6a70b26210fe0d862
Henit3 <henit33@gmail.com>
2024-04-28 12:57:09 +0100

    Fix bug where some player indices may be skipped when adding to network list/map

--- src/Extensions/PlayerControllerBExtensions.cs
+++ src/Extensions/PlayerControllerBExtensions.cs
@@ -4,7 +4,7 @@ namespace DramaMask.Extensions;
 
 public static class PlayerControllerBExtensions
 {
-    public static ulong GetId(this PlayerControllerB player) => player?.actualClientId ?? 0;
+    public static ulong GetId(this PlayerControllerB player) => player?.playerClientId ?? 0;
     public static bool IsLocal(this PlayerControllerB player) => player.GetId() == (player?.NetworkManager?.LocalClientId ?? 0);
 
     // We assume that the HoldMask animation has been overriden by the attach mask handler
