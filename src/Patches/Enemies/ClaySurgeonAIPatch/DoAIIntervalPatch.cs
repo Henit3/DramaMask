89aec2610749997ea3a02ab6183d52bb467145d6
Henit3 <henit33@gmail.com>
2024-07-13 23:08:22 +0100

    Fix ClaySurgeonAI support

--- src/Patches/Enemies/ClaySurgeonAIPatch/DoAIIntervalPatch.cs
+++ src/Patches/Enemies/ClaySurgeonAIPatch/DoAIIntervalPatch.cs
@@ -14,15 +14,19 @@ public class DoAIIntervalPatch
 
         // Match for last check on targetPlayer (before re-assignment on failed TargetClosestPlayer)
         matcher.End();
-        matcher.MatchBack(false, [new(OpCodes.Brfalse_S)]);
+        matcher.MatchBack(false, [new(OpCodes.Brfalse)]);
         var skipReassignmentTarget = matcher.Instruction.operand;
+        matcher.Advance(1);
 
-        // if (!PlayerIsTargetable(targetPlayer)) return;
+        // if (!this.PlayerIsTargetable(targetPlayer)) return;
         matcher.InsertAndAdvance([
+            new(OpCodes.Ldarg_0),       // this
             new(OpCodes.Ldloc_0),       // targetPlayer
+            new(OpCodes.Ldc_I4_0),      // false
+            new(OpCodes.Ldc_I4_0),      // false
             new(OpCodes.Call,           // IsThreatHiddenPlayer()
                 AccessTools.Method(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))),
-            new(OpCodes.Brfalse,         // <skip next if branch>
+            new(OpCodes.Brfalse,        // <skip next if branch>
                 skipReassignmentTarget)
         ]);
 
