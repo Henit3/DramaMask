0b2e79e3dfb7c5b6e84abc848e5c16ddf114140d
Henit3 <henit33@gmail.com>
2024-07-13 15:57:13 +0100

    Initial support for ClaySurgeonAI

---
+++ src/Patches/Enemies/ClaySurgeonAIPatch/DoAIIntervalPatch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace DramaMask.Patches.Enemies.ClaySurgeonAIPatch;
+
+[HarmonyPatch(typeof(ClaySurgeonAI), nameof(ClaySurgeonAI.DoAIInterval))]
+public class DoAIIntervalPatch
+{
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> HandleNonTargetablePlayer(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+    {
+        var matcher = new CodeMatcher(instructions);
+
+        // Match for last check on targetPlayer (before re-assignment on failed TargetClosestPlayer)
+        matcher.End();
+        matcher.MatchBack(false, [new(OpCodes.Brfalse_S)]);
+        var skipReassignmentTarget = matcher.Instruction.operand;
+
+        // if (!PlayerIsTargetable(targetPlayer)) return;
+        matcher.InsertAndAdvance([
+            new(OpCodes.Ldloc_0),       // targetPlayer
+            new(OpCodes.Call,           // IsThreatHiddenPlayer()
+                AccessTools.Method(typeof(EnemyAI), nameof(EnemyAI.PlayerIsTargetable))),
+            new(OpCodes.Brfalse,         // <skip next if branch>
+                skipReassignmentTarget)
+        ]);
+
+        return matcher.InstructionEnumeration();
+    }
+}
